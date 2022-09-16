27f9f42b7363fcae57a1696c0caab83d46c3bffb
comlekahmetcan <@comlekahmetcan.norepyl.github.com>
2022-09-16 14:57:02 +0300

    Interface created

---
+++ DataAccessLayer/Abstract/IBlogDal.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IBlogDal
+    {
+        List<Blog> ListAllCategory();
+        void AddCategory(Blog blog);
+        void DeleteCategory(Blog blog);
+        void UpdateCategory(Blog blog);
+        Blog GetById(int id);
+    }
+}
