bdf34112ee714fd777c8c532479006b8a98bbf73
comlekahmetcan <@comlekahmetcan.norepyl.github.com>
2022-09-16 16:28:30 +0300

    Entity Framework Repository created

--- DataAccessLayer/Abstract/IBlogDal.cs
+++ DataAccessLayer/Abstract/IBlogDal.cs
@@ -7,12 +7,8 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Abstract
 {
-    public interface IBlogDal
+    public interface IBlogDal : IGenericDal<Blog>
     {
-        List<Blog> ListAllCategory();
-        void AddCategory(Blog blog);
-        void DeleteCategory(Blog blog);
-        void UpdateCategory(Blog blog);
-        Blog GetById(int id);
+
     }
 }
