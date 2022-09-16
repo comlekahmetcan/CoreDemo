7b304921dce0abaa4223b873769b0b82f4293fd3
comlekahmetcan <@comlekahmetcan.norepyl.github.com>
2022-09-16 12:22:56 +0300

    Entity Framework Core packages added.

--- EntityLayer/Concrete/Category.cs
+++ EntityLayer/Concrete/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace EntityLayer.Concrete
 {
     public class Category
     {
+        [Key]
         //Erişim Belirleyici Türü - Değişken Türü İsim{get set}
         public int CategoryId { get; set; }
         public string CategoryName { get; set; }
