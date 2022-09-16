21c346d6a7472707f6838b4eaee96e388dbf7fcd
comlekahmetcan <@comlekahmetcan.norepyl.github.com>
2022-09-16 12:09:52 +0300

    Entity Layer created.

---
+++ EntityLayer/Concrete/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Category
+    {
+        //Erişim Belirleyici Türü - Değişken Türü İsim{get set}
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string CategoryDescription { get; set; }
+        public bool CategoryStatus { get; set; }
+
+    }
+}
