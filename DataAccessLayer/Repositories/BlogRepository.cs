c3fb449d29bbb0a922c54e4bee818d594c7abae3
comlekahmetcan <@comlekahmetcan.norepyl.github.com>
2022-09-16 15:18:01 +0300

    Repositories added

---
+++ DataAccessLayer/Repositories/BlogRepository.cs
@@ -0,0 +1,48 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class BlogRepository : IBlogDal
+    {
+        public void AddCategory(Blog blog)
+        {
+            using var c = new Context();
+            c.Add(blog);
+            c.SaveChanges();
+        }
+
+        public void DeleteCategory(Blog blog)
+        {
+            using var c = new Context();
+            c.Remove(blog);
+            c.SaveChanges();
+        }
+
+        public Blog GetById(int id)
+        {
+            using var c = new Context();
+            return c.Blogs.Find();
+        }
+
+        public List<Blog> ListAllCategory()
+        {
+            using var c = new Context();
+            return c.Blogs.ToList();
+        }
+
+        public void UpdateCategory(Blog blog)
+        {
+            using var c = new Context();
+            c.Update(blog);
+            c.SaveChanges();
+        }
+    }
+}
