522c15f0b042a06914c9c60ade04d5f0de9ec4d5
comlekahmetcan <@comlekahmetcan.norepyl.github.com>
2022-09-16 14:47:35 +0300

    Relationships added.(Blog-Category,Blog-Comment)

--- EntityLayer/Concrete/Category.cs
+++ EntityLayer/Concrete/Category.cs
@@ -16,5 +16,6 @@ namespace EntityLayer.Concrete
         public string CategoryDescription { get; set; }
         public bool CategoryStatus { get; set; }
 
+        public List<Blog> Blogs { get; set; }
     }
 }
