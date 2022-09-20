65b9c77dc2a03aa36b13936909dac8ba36786d6a
comlekahmetcan <@comlekahmetcan.norepyl.github.com>
2022-09-20 10:30:45 +0300

    Comment list for blogs

---
+++ CoreDemo/ViewComponents/Comment/CommentListByBlog.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.ViewComponents.Comment
+{
+    public class CommentListByBlog : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            CommentManager cm = new CommentManager(new EfCommentRepository());
+            var values = cm.GetList(4);
+            return View(values);
+        }
+    }
+}
