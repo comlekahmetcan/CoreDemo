c9e3315fd529041d663767df57e0e0cf9d62bd6b
comlekahmetcan <@comlekahmetcan.norepyl.github.com>
2022-09-20 10:12:28 +0300

    View Component created

---
+++ CoreDemo/Models/UserComment.cs
@@ -0,0 +1,9 @@
+﻿namespace CoreDemo.Models
+{
+    public class UserComment
+    {
+        public int ID { get; set; }
+        public string Username { get; set; }
+
+    }
+}
---
+++ CoreDemo/ViewComponents/CommentList.cs
@@ -0,0 +1,32 @@
+﻿using CoreDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CoreDemo.ViewComponents
+{
+    public class CommentList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var commentvalues = new List<UserComment>
+            {
+                new UserComment
+                {
+                    ID = 1,
+                    Username="AhmetCan"
+                },
+                new UserComment
+                {
+                    ID = 2,
+                    Username="Caner"
+                },
+                new UserComment
+                {
+                    ID = 2,
+                    Username="Nejat"
+                }
+            };
+            return View(commentvalues);
+        }
+    }
+}
