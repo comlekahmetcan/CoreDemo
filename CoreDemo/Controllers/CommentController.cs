f640fbeef9e2fedb7e9bc0c4239daf56137e400d
comlekahmetcan <@comlekahmetcan.norepyl.github.com>
2022-09-20 09:52:46 +0300

    Comment methods created(error)

--- CoreDemo/Controllers/CommentController.cs
+++ CoreDemo/Controllers/CommentController.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Controllers
 {
     public class CommentController : Controller
     {
+        CommentManager cm = new CommentManager(new EfCommentRepository());
         public IActionResult Index()
         {
             return View();
@@ -12,9 +15,10 @@ namespace CoreDemo.Controllers
         {
             return PartialView();
         }
-        public PartialViewResult CommentListByBlog()
+        public PartialViewResult CommentListByBlog(int id)
         {
-            return PartialView();
+            var values = cm.GetList(id);
+            return PartialView(values);
         }
     }
 }
--- DataAccessLayer/Abstract/ICommentDal.cs
+++ DataAccessLayer/Abstract/ICommentDal.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Abstract
 {
-    internal interface ICommentDal : IGenericDal<Comment>
+    public interface ICommentDal : IGenericDal<Comment>
     {
     }
 }
