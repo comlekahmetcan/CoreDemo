931063dd2f08ecd36ff8e38d1bc9dc41304d5147
comlekahmetcan <@comlekahmetcan.norepyl.github.com>
2022-09-21 11:45:49 +0300

    Writer register added and sweet alert use

--- CoreDemo/Controllers/RegisterController.cs
+++ CoreDemo/Controllers/RegisterController.cs
@@ -1,18 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Controllers
 {
     public class RegisterController : Controller
     {
+        WriterManager wm = new WriterManager(new EfWriterRepository());
         [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
         [HttpPost]
-        public IActionResult Index()
+        public IActionResult Index(Writer p)
         {
-            return View();
+            p.WriterStatus = true;
+            p.WriterAbout = "Deneme Test";
+            wm.WriterAdd(p);
+            return RedirectToAction("Index","Blog");
         }
     }
 }
