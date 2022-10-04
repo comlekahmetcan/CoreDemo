6307db3901e9439f377ddf85f41e67ae227d6c00
comlekahmetcan <@comlekahmetcan.norepyl.github.com>
2022-10-04 13:34:25 +0300

    E-mail newsletter created

---
+++ CoreDemo/Controllers/NewsLetterController.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+    public class NewsLetterController : Controller
+    {
+        NewsLetterManager nm = new NewsLetterManager(new EfNewsLetterRepository());
+        [HttpGet]
+        public PartialViewResult SubsribeMail()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public PartialViewResult SubscribeMail(NewsLetter p)
+        {
+            p.MailStatus = true;
+            nm.AddNewsLetter(p);
+            return PartialView();
+        }
+    }
+}
