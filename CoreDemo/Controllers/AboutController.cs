ab2e473584b46d7101658b3a111ac546ae843d38
comlekahmetcan <@comlekahmetcan.norepyl.github.com>
2022-10-04 15:54:35 +0300

    About created(eroor)

---
+++ CoreDemo/Controllers/AboutController.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+    public class AboutController : Controller
+    {
+        AboutManager abm = new AboutManager(new EfAboutRepository());
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult SocialMediaAbout()
+        {
+            var values = abm.GetList();
+            return PartialView(values);
+        }
+    }
+}
