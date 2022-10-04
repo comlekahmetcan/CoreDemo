931cd6e49ae1c5f9b11404cea67c295eafe62ee3
comlekahmetcan <@comlekahmetcan.norepyl.github.com>
2022-10-04 17:31:26 +0300

    Project level Authorize and Startup.cs Configure Service

--- CoreDemo/Controllers/LoginController.cs
+++ CoreDemo/Controllers/LoginController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Controllers
 {
     public class LoginController : Controller
     {
+        [AllowAnonymous]
         public IActionResult Index()
         {
             return View();
--- CoreDemo/Controllers/WriterController.cs
+++ CoreDemo/Controllers/WriterController.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Controllers
 {
-    [Authorize]
     public class WriterController : Controller
     {
         public IActionResult Index()
