be430848caad4d3042c6e1cb321377e7e4dca9a2
comlekahmetcan <@comlekahmetcan.norepyl.github.com>
2022-10-04 13:05:08 +0300

    Author's other writings and author blog relationship

--- BusinessLayer/Concrete/BlogManager.cs
+++ BusinessLayer/Concrete/BlogManager.cs
@@ -52,5 +52,10 @@ namespace BusinessLayer.Concrete
         {
             return _blogDal.GetListAll();
         }
+
+        public List<Blog> GetByBlogListWithWriter(int id)
+        {
+            return _blogDal.GetListAll(x => x.WriterId == id);
+        }
     }
 }
---
+++ CoreDemo/ViewComponents/Blog/WriterLastBlog.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.ViewComponents.Blog
+{
+    public class WriterLastBlog : ViewComponent
+    {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+        public IViewComponentResult Invoke()
+        {
+            var values = bm.GetByBlogListWithWriter(1);
+            return View(values);
+        }
+    }
+}
