f8852e5fadc680678e226e56c2a6131068b27a5c
comlekahmetcan <@comlekahmetcan.norepyl.github.com>
2022-10-04 16:46:34 +0300

    Contact page created

--- CoreDemo/Controllers/AboutController.cs
+++ CoreDemo/Controllers/AboutController.cs
@@ -9,12 +9,12 @@ namespace CoreDemo.Controllers
         AboutManager abm = new AboutManager(new EfAboutRepository());
         public IActionResult Index()
         {
-            return View();
+            var values = abm.GetList();
+            return View(values);
         }
         public PartialViewResult SocialMediaAbout()
         {
-            var values = abm.GetList();
-            return PartialView(values);
+            return PartialView();
         }
     }
 }
