f64f991c9df91766e15b1a72cb69f6117db47d31
comlekahmetcan <@comlekahmetcan.norepyl.github.com>
2022-10-05 12:54:29 +0300

    Writer panel theme added

--- CoreDemo/Controllers/WriterController.cs
+++ CoreDemo/Controllers/WriterController.cs
@@ -19,5 +19,11 @@ namespace CoreDemo.Controllers
         {
             return View();
         }
+
+        [AllowAnonymous]
+        public IActionResult Test()
+        {
+            return View();
+        }
     }
 }
