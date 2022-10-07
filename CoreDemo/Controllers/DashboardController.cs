164837d58d63aef7bb38e23aff5154f7e36c39dd
comlekahmetcan <@comlekahmetcan.norepyl.github.com>
2022-10-07 15:40:26 +0300

    Dashboard statistics and last ten blog revised

--- CoreDemo/Controllers/DashboardController.cs
+++ CoreDemo/Controllers/DashboardController.cs
@@ -1,7 +1,9 @@
 ﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace CoreDemo.Controllers
 {
@@ -12,7 +14,10 @@ namespace CoreDemo.Controllers
         [AllowAnonymous]
         public IActionResult Index()
         {
-
+            Context c = new Context();
+            ViewBag.v1 = c.Blogs.Count().ToString();
+            ViewBag.v2 = c.Blogs.Where(x => x.WriterId == 1).Count();
+            ViewBag.v3 = c.Categories.Count();
             return View();
         }
     }
