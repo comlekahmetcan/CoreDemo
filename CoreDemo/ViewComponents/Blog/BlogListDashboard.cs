cc89206d8f8a4c03ec1723f0da22a2f8944805eb
comlekahmetcan <@comlekahmetcan.norepyl.github.com>
2022-10-07 10:38:04 +0300

    Dashboard revised

---
+++ CoreDemo/ViewComponents/Blog/BlogListDashboard.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.ViewComponents.Blog
+{
+    public class BlogListDashboard:ViewComponent
+    {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+        public IViewComponentResult Invoke()
+        {
+            var values = bm.GetBlogListWithCategory();
+            return View(values);
+        }
+    }
+}
