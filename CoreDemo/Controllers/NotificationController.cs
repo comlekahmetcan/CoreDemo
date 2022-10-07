788b7e4c69361ddba26e8f3cfca6e177759a7807
comlekahmetcan <@comlekahmetcan.norepyl.github.com>
2022-10-07 17:13:58 +0300

    Notification class created

---
+++ CoreDemo/Controllers/NotificationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+    public class NotificationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CoreDemo/ViewComponents/Notification/NotificationList.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.ViewComponents.Notification
+{
+    public class NotificationList:ViewComponent
+    {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+        public IViewComponentResult Invoke()
+        {
+            var values = bm.GetLast3Blog();
+            return View(values);
+        }
+    }
+}
