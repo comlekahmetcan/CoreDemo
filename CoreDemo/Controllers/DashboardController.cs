7c9f1eae2b86f6c21bf10c0402ec7c3c8f6b2bf8
comlekahmetcan <@comlekahmetcan.norepyl.github.com>
2022-10-07 12:44:26 +0300

    Dashboard revised(about on dashboard)

--- CoreDemo/Controllers/DashboardController.cs
+++ CoreDemo/Controllers/DashboardController.cs
@@ -1,13 +1,18 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Controllers
 {
     public class DashboardController : Controller
     {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+
         [AllowAnonymous]
         public IActionResult Index()
         {
+
             return View();
         }
     }
---
+++ CoreDemo/ViewComponents/Writer/WriterAboutOnDashboard.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.ViewComponents.Writer
+{
+    public class WriterAboutOnDashboard : ViewComponent
+    {
+        WriterManager wm = new WriterManager(new EfWriterRepository());
+        public IViewComponentResult Invoke()
+        {
+            var values = wm.GetWriterById(1);
+            return View(values);
+        }
+    }
+}
