8bf7ac7f22a8670c8760585a3416ffc5ef1c6c85
comlekahmetcan <@comlekahmetcan.norepyl.github.com>
2022-10-07 10:59:16 +0300

    Dashboard revised(project status)

---
+++ CoreDemo/ViewComponents/Category/CategoryListDashboard.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.ViewComponents.Category
+{
+    public class CategoryListDashboard : ViewComponent
+    {
+        CategoryManager cm = new CategoryManager(new EfCategoryRepository());
+        public IViewComponentResult Invoke()
+        {
+            var values = cm.GetList();
+            return View(values);
+        }
+    }
+}
