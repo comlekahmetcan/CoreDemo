bb7168ce9413a8c48e7056de10431855905744c9
comlekahmetcan <@comlekahmetcan.norepyl.github.com>
2022-10-12 16:21:43 +0300

    Message class architecture completed

---
+++ BusinessLayer/Concrete/MessageManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class MessageManager : IMessageService
+    {
+        IMessageDal _messageDal;
+
+        public MessageManager(IMessageDal messageDal)
+        {
+            _messageDal = messageDal;
+        }
+
+        public List<Message> GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TAdd(Message t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(Message t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Message TGetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Message t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EntityLayer/Concrete/Message.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Message
+    {
+        public int MessageID { get; set; }
+        public string Sender { get; set; }
+        public string Receiver { get; set; }
+        public string Subject { get; set; }
+        public string MessageDetails { get; set; }
+        public DateTime MessageDate { get; set; }
+        public bool MessageStatus { get; set; }
+
+    }
+}
