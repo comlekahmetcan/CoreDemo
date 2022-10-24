74d195a6c3885ec71e5a47eccaa870e1b94c5b2a
comlekahmetcan <@comlekahmetcan.norepyl.github.com>
2022-10-24 15:25:39 +0300

    Listing Notifications and task(date difference)

--- CoreDemo/Controllers/NotificationController.cs
+++ CoreDemo/Controllers/NotificationController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Controllers
 {
     public class NotificationController : Controller
     {
+        NotificationManager nm = new NotificationManager(new EfNotificationRepository());
         public IActionResult Index()
         {
             return View();
+        }
+        [AllowAnonymous]
+        public IActionResult AllNotification()
+        {
+            var values = nm.GetList();
+            return View(values);
         }
     }
 }
