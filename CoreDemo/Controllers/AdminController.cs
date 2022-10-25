c7d49a2c7367be898d3ea371b04799026063f055
comlekahmetcan <@comlekahmetcan.norepyl.github.com>
2022-10-25 12:51:32 +0300

    Admin Layout Editing

--- CoreDemo/Controllers/AdminController.cs
+++ CoreDemo/Controllers/AdminController.cs
@@ -8,5 +8,9 @@ namespace CoreDemo.Controllers
         {
             return View();
         }
+        public PartialViewResult AdminNavbarPartial()
+        {
+            return PartialView();
+        }
     }
 }
