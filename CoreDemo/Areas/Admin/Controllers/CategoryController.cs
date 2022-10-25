98b8d3c26bb9fcf485e424981ae643fe5d2927bc
comlekahmetcan <@comlekahmetcan.norepyl.github.com>
2022-10-25 15:06:57 +0300

    Areas Map Controller Route

---
+++ CoreDemo/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Areas.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        [Area("Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
