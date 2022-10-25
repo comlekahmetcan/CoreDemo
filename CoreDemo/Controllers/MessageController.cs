18b6b0da6903e205b73dd70c7d5750dcfa98106b
comlekahmetcan <@comlekahmetcan.norepyl.github.com>
2022-10-25 11:18:27 +0300

    Listing message details

---
+++ CoreDemo/Controllers/MessageController.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+    [AllowAnonymous]
+    public class MessageController : Controller
+    {
+        Message2Manager mm = new Message2Manager(new EfMessage2Repository());
+
+        public IActionResult InBox()
+        {
+            int id = 1;
+            var values = mm.GetInboxListByWriter(id);
+            return View(values);
+        }
+        public IActionResult MessageDetails(int id)
+        {
+            var value = mm.TGetById(id);
+            return View(value);
+        }
+    }
+}
