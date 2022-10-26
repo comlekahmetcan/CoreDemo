87b89525a25a6c1222d3c31f406a1090de148e87
comlekahmetcan <@comlekahmetcan.norepyl.github.com>
2022-10-26 15:51:14 +0300

    Statistic 3 and Admin arthictecture created

--- CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic4.cs
+++ CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic4.cs
@@ -1,11 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
 {
     public class Statistic4 : ViewComponent
     {
+        Context c = new Context();
         public IViewComponentResult Invoke()
         {
+            ViewBag.v1 = c.Admins.Where(x => x.AdminID == 1).Select(y => y.Name).FirstOrDefault();
+            ViewBag.v2 = c.Admins.Where(x => x.AdminID == 1).Select(y => y.ImageURL).FirstOrDefault();
+            ViewBag.v3 = c.Admins.Where(x => x.AdminID == 1).Select(y => y.ShortDescription).FirstOrDefault();
             return View();
         }
     }
