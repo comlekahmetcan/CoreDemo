dd20adcfad8993b47a0a979fe3ccc4a328c7e276
comlekahmetcan <@comlekahmetcan.norepyl.github.com>
2022-10-26 14:56:39 +0300

    Statistic 2

---
+++ CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic2.cs
@@ -0,0 +1,16 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
+{
+    public class Statistic2 : ViewComponent
+    {
+        Context c = new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.v1 = c.Blogs.OrderByDescending(x => x.BlogID).Select(x => x.BlogTitle).Take(1).FirstOrDefault();
+            return View();
+        }
+    }
+}
