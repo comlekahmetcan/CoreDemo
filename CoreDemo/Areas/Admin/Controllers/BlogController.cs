f8ad43a507efeca906779d67b29fc9cf8b8352f9
comlekahmetcan <@comlekahmetcan.norepyl.github.com>
2022-10-26 13:47:38 +0300

    Excel Dynamic Table Export

--- CoreDemo/Areas/Admin/Controllers/BlogController.cs
+++ CoreDemo/Areas/Admin/Controllers/BlogController.cs
@@ -1,10 +1,12 @@
 ﻿using ClosedXML.Excel;
 using CoreDemo.Areas.Admin.Models;
+using DataAccessLayer.Concrete;
 using DocumentFormat.OpenXml.Office2010.ExcelAc;
 using DocumentFormat.OpenXml.Spreadsheet;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Linq.Expressions;
 
 namespace CoreDemo.Areas.Admin.Controllers
@@ -48,6 +50,43 @@ namespace CoreDemo.Areas.Admin.Controllers
         }
 
         public IActionResult BlogListExcel()
+        {
+            return View();
+        }
+
+        public IActionResult ExportDynamicExcelBlogList()
+        {
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Blog Listesi");
+                worksheet.Cell(1, 1).Value = "Blog ID";
+                worksheet.Cell(1, 2).Value = "Blog Adı";
+
+                int BlogRowCount = 2;
+                foreach (var item in BlogTitleList())
+                {
+                    worksheet.Cell(BlogRowCount, 1).Value = item.ID;
+                    worksheet.Cell(BlogRowCount, 2).Value = item.BlogName;
+                    BlogRowCount++;
+                }
+                using (var stream = new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+                    var content = stream.ToArray();
+                    return File(content, "application/vnd.openxmlformats-officedocument.spereadsheetml.sheet", "Calimsa1.xlsx");
+                }
+            }
+        }
+        public List<BlogModel2> BlogTitleList()
+        {
+            List<BlogModel2> bm = new List<BlogModel2>();
+            using (var c = new Context())
+            {
+                bm = c.Blogs.Select(x => new BlogModel2 { ID = x.BlogID, BlogName = x.BlogTitle }).ToList();
+            }
+            return bm;
+        }
+        public IActionResult BlogTitleListExcel()
         {
             return View();
         }
