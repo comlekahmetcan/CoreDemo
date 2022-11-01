ee88196d3c1c56ea8f5ad0b74c2111f5e93b87b3
AhmetCan ÇÖMLEK <ahmetcan.comlek@cts.com.tr>
2022-11-01 16:19:47 +0300

    Pulling Weather Information via Api(https://openweathermap.org/current)xml fortmat

--- CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
+++ CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -4,6 +4,7 @@ using DataAccessLayer.EntityFramework;
 using DocumentFormat.OpenXml.Packaging;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
+using System.Xml.Linq;
 
 namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
 {
@@ -16,6 +17,11 @@ namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
             ViewBag.v1 = bm.GetList().Count();
             ViewBag.v2 = c.Contacts.Count();
             ViewBag.v3 = c.Comments.Count();
+
+            string api = "d489781ffcda5dfe7514528c542f6ac0";
+            string connection = "https://api.openweathermap.org/data/2.5/weather?q=manisa&mode=xml&lang=tr&units=metric&appid=" + api;
+            XDocument document = XDocument.Load(connection);
+            ViewBag.v4 = document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
             return View();
         }
     }
