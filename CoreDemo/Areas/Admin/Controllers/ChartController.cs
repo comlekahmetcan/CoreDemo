18901239044fbb595699dd5fbdac53f3ff5cfd71
AhmetCan ÇÖMLEK <ahmetcan.comlek@cts.com.tr>
2022-11-01 16:34:09 +0300

    Google Chart 1

---
+++ CoreDemo/Areas/Admin/Controllers/ChartController.cs
@@ -0,0 +1,38 @@
+﻿using CoreDemo.Areas.Admin.Models;
+using DocumentFormat.OpenXml.Office2010.ExcelAc;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CoreDemo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ChartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CategoryChart()
+        {
+            List<CategoryClass> list = new List<CategoryClass>();
+
+            list.Add(new CategoryClass
+            {
+                categoryname = "Teknoloji",
+                categorycount = 10
+            });
+            list.Add(new CategoryClass
+            {
+                categoryname = "Yazılım",
+                categorycount = 14
+            });
+            list.Add(new CategoryClass
+            {
+                categoryname = "Spor",
+                categorycount = 5
+            });
+            return Json(new { jsonlist = list });
+        }
+    }
+}
---
+++ CoreDemo/Areas/Admin/Models/CategoryClass.cs
@@ -0,0 +1,9 @@
+﻿namespace CoreDemo.Areas.Admin.Models
+{
+    public class CategoryClass
+    {
+        public int categorycount { get; set; }
+        public string categoryname { get; set; }
+
+    }
+}
