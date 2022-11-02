787472ce44e326f2a4f18ecd354aaeccf01d4c8e
AhmetCan ÇÖMLEK <ahmetcan.comlek@cts.com.tr>
2022-11-02 11:29:58 +0300

    Deleting with Ajax

--- CoreDemo/Areas/Admin/Controllers/WriterController.cs
+++ CoreDemo/Areas/Admin/Controllers/WriterController.cs
@@ -39,6 +39,13 @@ namespace CoreDemo.Areas.Admin.Controllers
             return Json(jsonWriters);
         }
 
+        public IActionResult DeleteWriter(int id)
+        {
+            var writer = writers.FirstOrDefault(x => x.Id == id);
+            writers.Remove(writer);
+            return Json(writer);
+        }
+
         public static List<WriterClass> writers = new List<WriterClass>
         {
             new WriterClass
